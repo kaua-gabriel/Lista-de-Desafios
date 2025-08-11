2868f55af6026cf38210615f21b6300d88e5fce8
KAUA GABRIEL SILVA <56428102862@info.local>
2025-08-11 15:34:11 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const desafios = {
+    "1": {
+    "titulo": "Desafio 1: Protótipo de Jogo com Inteligência Artificial",
+    "descricao": "Curso de Jogos Digitais — Desenvolva um protótipo de jogo com pelo menos uma mecânica de gameplay (ex: combate, puzzle ou exploração) e implemente uma inteligência artificial básica para o inimigo ou NPC. A IA deve tomar decisões baseadas no comportamento do jogador. Envie um vídeo demonstrando a IA em ação e o link do projeto."
+  }
+}
+
+function carregarDesafios() {
+
+    const container = document.getElementById('ListaDesafios');
+
+    for (let desafioID in desafios) {
+
+        const desafioAtual = desafios[desafioID]
+
+        const elementoDesafio = document.createElement('div')
+
+        elementoDesafio.className = 'desafio';
+
+        elementoDesafio.innerHTML = `
+    <h3>${desafioAtual.titulo}</h3>
+    <p>${desafioAtual.descricao}</p>
+  `;
+
+        container.appendChild(elementoDesafio);
+    }
+}
+
+function exibirDesafioPrincipal() {
+    
+    const parametros = new URLSearchParams(window.location.search);
+
+    const id = parametros.get("id");
+
+    if (id && desafios[id]) {
+        const desafio = desafios[id];
+
+        document.getElementById("modalTitulo").innerText = desafio.titulo;
+        document.getElementById("modalDescricao").innerText = desafio.descricao;
+    
+        document.getElementById("modal").style.display = "block";
+    }
+}
+
+function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.onload = () => {
+    carregarDesafios();
+    exibirDesafioPrincipal();
+}
