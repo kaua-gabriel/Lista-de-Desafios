a68597d3c0f53494c58f8355f5bda5ec65d054e3
gkaua <166552306+kaua-gabriel@users.noreply.github.com>
2025-08-11 15:43:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const desafios = {
+
     "1": {
-    "titulo": "Desafio 1: Protótipo de Jogo com Inteligência Artificial",
-    "descricao": "Curso de Jogos Digitais — Desenvolva um protótipo de jogo com pelo menos uma mecânica de gameplay (ex: combate, puzzle ou exploração) e implemente uma inteligência artificial básica para o inimigo ou NPC. A IA deve tomar decisões baseadas no comportamento do jogador. Envie um vídeo demonstrando a IA em ação e o link do projeto."
-  }
+        "titulo": "Desafio 1: Protótipo de Jogo com Inteligência Artificial",
+        "descricao": "Curso de Jogos Digitais — Desenvolva um protótipo de jogo com pelo menos uma mecânica de gameplay (ex: combate, puzzle ou exploração) e implemente uma inteligência artificial básica para o inimigo ou NPC. A IA deve tomar decisões baseadas no comportamento do jogador. Envie um vídeo demonstrando a IA em ação e o link do projeto."
+    }
+
 }
 
 function carregarDesafios() {
 
-    const container = document.getElementById('ListaDesafios');
+    const container = document.getElementById('listaDesafios')
 
-    for (let desafioID in desafios) {
+    for (let desafiosID in desafios) {
 
-        const desafioAtual = desafios[desafioID]
+        const desafioAtual = desafios[desafiosID];
 
-        const elementoDesafio = document.createElement('div')
+        const elementoDesafio = document.createElement('div');
 
         elementoDesafio.className = 'desafio';
 
@@ -22,31 +24,35 @@ function carregarDesafios() {
     <p>${desafioAtual.descricao}</p>
   `;
 
-        container.appendChild(elementoDesafio);
+        container.appendChild(elementoDesafio)
+
     }
 }
 
 function exibirDesafioPrincipal() {
-    
-    const parametros = new URLSearchParams(window.location.search);
 
-    const id = parametros.get("id");
+    const parematros = new URLSearchParams(window.location.search);
+
+    const id = parematros.get('id');
 
     if (id && desafios[id]) {
-        const desafio = desafios[id];
+        const desafio = desafio[id];
 
         document.getElementById("modalTitulo").innerText = desafio.titulo;
         document.getElementById("modalDescricao").innerText = desafio.descricao;
-    
-        document.getElementById("modal").style.display = "block";
+
+        document.getElementById("modal").style.display = "blocks";
     }
 }
 
+
 function fecharModal() {
+
     document.getElementById("modal").style.display = "none";
 }
 
-window.onload = () => {
+
+window.onload= () => {
     carregarDesafios();
     exibirDesafioPrincipal();
 }
